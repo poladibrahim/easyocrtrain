bdeaef1adf54e0ed7a978002c7138737a0963a35
poladibrahim <109878344+poladibrahim@users.noreply.github.com>
2024-06-20 13:29:39 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -287,8 +287,9 @@ class CTCLabelConverter(object):
             t = text_index[index:index + l]
             
             char_list = []
-            print("t is in decode_greedy",t)
             for indices in t:
+                if indices.dim() == 0:
+                    indices = indices.unsqueeze(0) 
                 for i in range(len(indices)):
                     if indices[i].item() not in self.ignore_idx and (i == 0 or indices[i].item() != indices[i - 1].item()):  # removing repeated characters and blank (and separator).
                         char_list.append(self.character[indices[i].item()])
